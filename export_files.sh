aa83f2d0f20d13034cd1e0bfbc0fbd682328a71e
Nick van Leeuwen <nickvanleeuwen01@gmail.com>
2023-03-18 16:58:01 +0100

    Fixed drill test

--- export_files.sh
+++ export_files.sh
@@ -38,10 +38,10 @@ elif [ "$2" = "pcb_gerbers" ]; then
   kicad-cli pcb export gerbers -l "$3" "$1"
   zip "${file_name}-gerbers.zip" *.g*
 elif [ "$2" = "pcb_drill" ]; then
-  kicad-cli pcb export dill "$1"
+  kicad-cli pcb export drill "$1"
 elif [ "$2" = "pcb_gerbers_drill" ]; then
   kicad-cli pcb export gerbers -l "$3" "$1"
-  kicad-cli pcb export dill "$1"
+  kicad-cli pcb export drill "$1"
   zip "${file_name}-gerbers.zip" *.g* *.drl
 else
   echo "::error::Type is not correct"
